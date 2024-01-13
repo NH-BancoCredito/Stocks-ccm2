b87fba280c1f86682fd32e1d6304a174707c27ee
javier <javietuga@gmail.com>
2024-01-12 21:50:20 -0300

    adicionar componentes de kafka para publicar en el topico

--- Stocks.Infrastructure/DependencyInjection.cs
+++ Stocks.Infrastructure/DependencyInjection.cs
@@ -9,6 +9,10 @@ using System.Threading.Tasks;
 using Stocks.Domain.Repositories;
 using Stocks.Infrastructure.Repositories;
 using MongoDB.Driver;
+using Confluent.Kafka;
+using Stocks.Domain.Service.Events;
+using Stocks.Infrastructure.Services.Events;
+using System.Net;
 
 
 namespace Stocks.Infrastructure
@@ -22,6 +26,8 @@ namespace Stocks.Infrastructure
 
             services.AddDataBaseFactories(connectionString);
             services.AddRepositories();
+            services.AddProducer();
+            services.AddEventServices();
         }
 
         private static void AddDataBaseFactories(this IServiceCollection services, string connectionString)
@@ -39,6 +45,24 @@ namespace Stocks.Infrastructure
             services.AddScoped<IProductoRepository, ProductoRepository>();
 
             
+        }
+
+        private static IServiceCollection AddProducer(this IServiceCollection services)
+        {
+            var config = new ProducerConfig
+            {
+                Acks = Acks.Leader,
+                BootstrapServers = "127.0.0.1:9093",
+                ClientId = Dns.GetHostName(),
+            };
+
+            services.AddSingleton<IPublisherFactory>(sp => new PublisherFactory(config));
+            return services;
+        }
+
+        private static void AddEventServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IEventSender, EventSender>();
         }
     }
 }
