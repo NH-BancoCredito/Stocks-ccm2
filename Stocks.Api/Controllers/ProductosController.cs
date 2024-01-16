57329621cc516bb6745facd217a1cf027ed74ee3
javier <javietuga@gmail.com>
2024-01-15 22:10:12 -0300

    adicionar llamada http a l microservicio de stock

--- Stocks.Api/Controllers/ProductosController.cs
+++ Stocks.Api/Controllers/ProductosController.cs
@@ -23,16 +23,16 @@ namespace Stocks.Api.Controllers
             return Ok(response);
         }
 
-        [HttpGet("reservar")]
-        public async Task<IActionResult> Reservar([FromQuery] ReservarStockRequest request)
+        [HttpPut("reservar")]
+        public async Task<IActionResult> Reservar([FromBody] ReservarStockRequest request)
         {
             var response = await _mediator.Send(request);
 
             return Ok(response);
         }
 
-        [HttpGet("reponer")]
-        public async Task<IActionResult> Reponer([FromQuery] ReponerStockRequest request)
+        [HttpPut("reponer")]
+        public async Task<IActionResult> Reponer([FromBody] ReponerStockRequest request)
         {
             var response = await _mediator.Send(request);
 
